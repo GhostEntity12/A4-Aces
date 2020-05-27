facce3aa0eb2f43d1a2c3230521edb96e18d93b5
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-05-27 12:27:03 +1000

    Menus and TMPro

---
+++ Assets/Scripts/AnimatorSetBool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSetBool : MonoBehaviour
+{
+    public Animator animator;
+    public string boolName;
+
+    public void SetBool(bool value)
+    {
+        animator.SetBool(boolName, value);
+    }
+
+    private void Awake()
+    {
+        transform.LookAt(Camera.main.transform.position);
+    }
+}
