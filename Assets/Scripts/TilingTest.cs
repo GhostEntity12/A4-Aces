b12870c5d158c831b23d391ac6733159386bca36
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-06-16 17:48:34 +1000

    Testing script for altassing textures
    will hopefully be used for planes

---
+++ Assets/Scripts/TilingTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class TilingTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tilingObject;
+
+    [SerializeField]
+    Vector2Int dimensions;
+
+    [Range(0,15)]
+    public int position;
+    
+    float xJump, yJump;
+
+    Renderer r;
+
+    private void Awake()
+    {
+        r = tilingObject.GetComponent<Renderer>();
+        xJump = 1f / dimensions.x;
+        yJump = 1f / dimensions.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            position = (position + 15) % 16;
+            UpdateTexture();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            position = (position + 17) % 16;
+            UpdateTexture();
+        }
+    }
+
+    void UpdateTexture()
+    {
+        if (position > dimensions.x * dimensions.y)
+        {
+            Debug.LogError($"{position} is out of range!");
+            return;
+        }
+        int xPos = Mathf.FloorToInt(position / dimensions.x);
+        int yPos = position % dimensions.y;
+        print($"{position} maps to: {xPos}, {yPos}");
+
+        r.material.SetTextureOffset("_MainTex", new Vector2(xPos * xJump, yPos * yJump));
+    }
+}
