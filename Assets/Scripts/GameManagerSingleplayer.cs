f36db8ab70889f84cabe4b793b3ae026432d25e1
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-06-17 10:16:40 +1000

    Tried to bake lightmaps

--- Assets/Scripts/GameManagerSingleplayer.cs
+++ Assets/Scripts/GameManagerSingleplayer.cs
@@ -8,13 +8,23 @@ public class GameManagerSingleplayer : MonoBehaviour
 {
     public static GameManagerSingleplayer instance;
 
-    public GameObject[] planes;
+    public GameObject plane;
 
     public List<GameObject> spawnPoints;
+
+    [Header("Tiling")]
+    [SerializeField]
+    Vector2Int dimensions;
+
+    float xJump, yJump;
+
+    Renderer r;
 
     private void Awake()
     {
         instance = this;
+        xJump = 1f / dimensions.x;
+        yJump = 1f / dimensions.y;
     }
 
     // Start is called before the first frame update
@@ -24,7 +34,11 @@ public class GameManagerSingleplayer : MonoBehaviour
 
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
 
-        GameObject player = Instantiate(planes[Random.Range(0, planes.Length)], spawnPoint.position, spawnPoint.rotation);
+        GameObject player = Instantiate(plane, spawnPoint.position, spawnPoint.rotation);
+        int position = Random.Range(0, dimensions.x * dimensions.y);
+        int xPos = Mathf.FloorToInt(position / dimensions.x);
+        int yPos = position % dimensions.y;
+        player.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(xPos * xJump, yPos * yJump));
         player.GetComponent<Player>().mode = Gamemode.Multiplayer;
     }
 
